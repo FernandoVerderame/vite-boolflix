dfe88fd2bedf8fa0027661d67337ed8400f3cd34
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-07 19:28:35 +0100

    added movies list template

--- src/components/movies/MoviesList.vue
+++ src/components/movies/MoviesList.vue
@@ -1,15 +1,18 @@
 <script>
+import { store } from '../../data/store'
 import MoviesCard from './MoviesCard.vue';
 
 export default {
     name: 'MoviesList',
 
-    components: { MoviesCard }
+    components: { MoviesCard },
+
+    data: () => store
 };
 </script>
 
 <template>
-    <MoviesCard />
+    <MoviesCard v-for="movie in movies" :key="movie.id" v-bind="movie" />
 </template>
 
 <style lang="scss" scoped></style>
