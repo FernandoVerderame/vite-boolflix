70a24102d0ac6f354930cebd53f68a450b727ed5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-08 17:36:56 +0100

    added logo image & navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,8 +11,28 @@ export default {
 </script>
 
 <template>
-    <SearchForm @submit-search="$emit('search-productions')" @term-change="$emit('term-change', $event)"
-        placeholder="Scrivi qui..." buttonLabel="Cerca" />
+    <header>
+        <div class="nav-bar d-flex justify-content-between align-items-center">
+            <div class="logo-image">
+                <img src="../assets/img/logo-boolflix.png" alt="logo">
+            </div>
+            <SearchForm @submit-search="$emit('search-productions')" @term-change="$emit('term-change', $event)"
+                placeholder="Scrivi qui..." buttonLabel="Cerca" />
+        </div>
+    </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    background-color: #EE3424;
+}
+
+.nav-bar {
+    width: 80vw;
+    margin: 0 auto;
+
+    img {
+        width: 250px;
+    }
+}
+</style>
