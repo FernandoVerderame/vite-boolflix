8785fe36df3fcad4300f4b29c466907ff72a3517
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-08 12:28:39 +0100

    refactoring production list

---
+++ src/components/movies/ProductionList.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../../data/store'
+import ProductionCard from './ProductionCard.vue';
+
+export default {
+    name: 'MoviesList',
+
+    components: { ProductionCard },
+
+    data: () => ({ store })
+};
+</script>
+
+<template>
+    <section>
+        <h2>Movies</h2>
+        <ProductionCard v-for="movie in store.movies" :key="movie.id" :production="movie" />
+    </section>
+
+    <section>
+        <h2>Series</h2>
+        <ProductionCard v-for="serie in store.series" :key="serie.id" :production="serie" />
+    </section>
+</template>
+
+<style lang="scss" scoped></style>
