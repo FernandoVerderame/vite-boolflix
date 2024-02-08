4c08a07d04c31bb9f3cfd5c412215f223ce46860
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-08 18:00:24 +0100

    added navbar links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,8 +13,21 @@ export default {
 <template>
     <header>
         <div class="nav-bar d-flex justify-content-between align-items-center">
-            <div class="logo-image">
-                <img src="../assets/img/logo-boolflix.png" alt="logo">
+            <div class="nav-left d-flex align-items-center ">
+                <div class="logo-image">
+                    <img src="../assets/img/logo-boolflix.png" alt="logo">
+                </div>
+
+                <div class="nav-links">
+                    <ul class="d-flex">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Serie TV</a></li>
+                        <li><a href="#">Film</a></li>
+                        <li><a href="#">Originali</a></li>
+                        <li><a href="#">Aggiunti di recente</a></li>
+                        <li><a href="#">La mia lista</a></li>
+                    </ul>
+                </div>
             </div>
             <SearchForm @submit-search="$emit('search-productions')" @term-change="$emit('term-change', $event)"
                 placeholder="Scrivi qui..." buttonLabel="Cerca" />
@@ -28,11 +41,25 @@ header {
 }
 
 .nav-bar {
-    width: 80vw;
+    width: 90vw;
     margin: 0 auto;
 
-    img {
-        width: 250px;
+    .nav-left {
+        img {
+            width: 250px;
+        }
+    }
+}
+
+ul li a {
+    font-weight: 600;
+    text-shadow: 0px 0px 8px #000;
+    width: 100%;
+    padding: 10px;
+
+    &:hover {
+        background-color: black;
+        transition: .4s;
     }
 }
 </style>
