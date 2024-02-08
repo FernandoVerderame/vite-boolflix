cc360ad936065b0d17754666d7749f447514ee13
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-08 11:09:18 +0100

    refactoring api

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,7 @@
-const apiUri = 'https://api.themoviedb.org/3/search/movie';
+const api = {
+    basiUri: 'https://api.themoviedb.org/3',
+    apiKey: 'adbe6c00f84dac0fa250a3f0e40cff0d',
+    language: 'it-IT'
+}
 
-export { apiUri }
+export { api }
